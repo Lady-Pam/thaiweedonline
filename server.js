9e4a27e94e8cba657a573b3f0eb2cfb66887c3d4
Lady-Pam <ladypam.cannabis@gmail.com>
2024-10-28 15:37:11 +0700

    server.js fix

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 // Trasa do formularza
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // Upewnij się, że masz plik index.html
+app.get('/formula', (req, res) => {
+    res.sendFile(__dirname + '/formula.html'); // Upewnij się, że masz plik formularz.html
   });
-
+// Trasa do formularza
 
 // Endpoint do obsługi przesyłania formularza
 app.post('/upload', upload.single('file'), (req, res) => {
